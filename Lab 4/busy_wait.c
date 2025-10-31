e11969dc6c2a770abdee83aa85815f98e25f692a
smokingkrillz <89337823+smokingkrillz@users.noreply.github.com>
2025-10-31 15:54:31 +0100

    fucked

--- Lab 4/busy_wait.c
+++ Lab 4/busy_wait.c
@@ -1,78 +1,77 @@
-#include <util/delay.h>
-#include <stdbool.h>
-#include <avr/io.h>
-
-#define LED_bm PIN2_bm
-void AC_init() {
-    // Configure pin 2 of PORT D as an output pin. See chapter 18.3.1 of the datasheet
-    PORTD.DIRCLR = PIN2_bm;
-
-        //PORTD.PINCONFIG = PORT_ISC_INPUT_DISABLE_gc | PORT_PULLUPEN_bm;
-        PORTB.PINCONFIG = PORT_ISC_INPUT_DISABLE_gc | PORT_PULLUPEN_bm;
-        PORTC.PINCONFIG = PORT_ISC_INPUT_DISABLE_gc | PORT_PULLUPEN_bm;
-        PORTE.PINCONFIG = PORT_ISC_INPUT_DISABLE_gc | PORT_PULLUPEN_bm;
-
-        PORTD.PIN2CTRL = PORT_ISC_INPUT_DISABLE_gc;
-    //PORTA.PINCTRLUPD = 0xFF;
-    PORTB.PINCTRLUPD = 0xFF;
-    PORTC.PINCTRLUPD = 0xFF;
-    //PORTD.PINCTRLUPD = 0xFF;
-PORTE.PINCTRLUPD = 0xFF;
+// #include <avr/io.h>
+// #include <stdbool.h>
+// #include <util/delay.h>
 
 
-    // Disable digital input buffer and pull-up resistor for pin PD2
-    // Set the positive input source to AINP0 and the negative input source to the DAC reference (DACREF)
-    AC0.MUXCTRL = AC_MUXPOS_AINP0_gc | AC_MUXNEG_DACREF_gc;
-    // Enable the Analog Comparator by writing AC_ENABLE_bm to the CTRLA register
-    AC0.DACREF = 25;
-    AC0.CTRLA = AC_OUTEN_bm | AC_ENABLE_bm;
+// #define LED_bm PIN2_bm
+// void AC_init() {
+//   // Configure pin 2 of PORT D as an output pin. See chapter 18.3.1 of the
+//   PORTD.DIRCLR = PIN2_bm;
+  
 
+//   // Disable input buffer and enable pull-up resistor
+//   PORTB.PINCONFIG = PORT_ISC_INPUT_DISABLE_gc | PORT_PULLUPEN_bm;
+//   PORTC.PINCONFIG = PORT_ISC_INPUT_DISABLE_gc | PORT_PULLUPEN_bm;
+//   PORTE.PINCONFIG = PORT_ISC_INPUT_DISABLE_gc | PORT_PULLUPEN_bm;
 
+//   PORTD.PIN2CTRL = PORT_ISC_INPUT_DISABLE_gc;
+//   PORTB.PINCTRLUPD = 0xFF;
+//   PORTC.PINCTRLUPD = 0xFF;
+//   // PORTD.PINCTRLUPD = 0xFF;
+//   PORTE.PINCTRLUPD = 0xFF;
 
-}
+//     //compare positive input (AIN0) to DAC reference
+//   AC0.MUXCTRL = AC_MUXPOS_AINP0_gc | AC_MUXNEG_DACREF_gc;
 
+//   AC0.DACREF = 25;
 
-void LED_init() {
-    PORTA.DIRSET = PIN2_bm;
-}
-void set_LED_on(){
-    // LED is active low. Set pin LOW to turn LED on
-    PORTA.OUTCLR = PIN2_bm;
-}
-void set_LED_off(){
-    // LED is active low. Set pin HIGH to turn LED off
-    PORTA.OUTSET = PIN2_bm;
-}
+//   // Enable output and the comparator
+//   AC0.CTRLA = AC_OUTEN_bm | AC_ENABLE_bm;
+// }
 
-bool AC_above_threshold() {
-    // Check the output of the Analog Comparator
-    return (bool)(AC0.STATUS & AC_CMPSTATE_bm);
-}
+// void LED_init() 
+// {
+//     // Configure pin 2 of PORT A as an output pin
+//     PORTA.DIRSET = PIN2_bm;
+// }
+// void set_LED_on() 
+// {
+//   // LED is active low. Set pin LOW to turn LED on
+//   PORTA.OUTCLR = PIN2_bm;
+// }
+// void set_LED_off() 
+// {
+//   // LED is active low. Set pin HIGH to turn LED off
+//   PORTA.OUTSET = PIN2_bm;
+// }
 
-void VREF_init(void) {
-    VREF.ACREF = VREF_REFSEL_1V024_gc;
-}
+// bool AC_above_threshold() 
+// {
+//   // Check the output of the Analog Comparator
+//   return (bool)(AC0.STATUS & AC_CMPSTATE_bm);
+// }
 
-int main() {
-    _PROTECTED_WRITE(CLKCTRL.MCLKCTRLB, CLKCTRL_PDIV_4X_gc | CLKCTRL_PEN_bm); // 4 MHz / 4
-    // Initialize Analog Comparator
-    AC_init();
-    VREF_init();
-    LED_init();
+// // set reference voltage to 1.024V
+// void VREF_init(void)
+// { 
+//     VREF.ACREF = VREF_REFSEL_1V024_gc; 
+// }
 
+// int main() 
+// {
+//   _PROTECTED_WRITE(CLKCTRL.MCLKCTRLB, CLKCTRL_PDIV_4X_gc | CLKCTRL_PEN_bm);
+//   // Initialize Analog Comparator
+//   AC_init();
+//   VREF_init();
+//   LED_init();
 
+//   while (1) {
+//     if (AC_above_threshold()) {
+//       set_LED_off();
 
-    while (1)
-    {
-        if (AC_above_threshold())
-        {
-            set_LED_off();
-            
-        }
-        else
-        {
-            set_LED_on();
-        }
-    }
-    return 0;
-}
+//     } else {
+//       set_LED_on();
+//     }
+//   }
+//   return 0;
+// }
