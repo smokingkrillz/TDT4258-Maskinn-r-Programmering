5db0c57bcf7ef02e2357c5309de86a49cb29d4cb
smokingkrillz <89337823+smokingkrillz@users.noreply.github.com>
2025-10-31 16:13:28 +0100

    works

--- Lab 4/polling.c
+++ Lab 4/polling.c
@@ -2,6 +2,7 @@
 #include <avr/sleep.h>
 #include <avr/interrupt.h>
 
+#define LED_bm PIN2_bm
 void AC_init() {
   // Configure pin 2 of PORT D as an output pin
 
@@ -14,7 +15,7 @@ void AC_init() {
   PORTE.PINCTRLUPD = 0xFF;
 
   // compare positive input (AIN0) to DAC reference
-  PORTD.DIRCLR = PIN2_bm;
+  PORTD.DIRCLR = LED_bm;
   PORTD.PIN2CTRL = PORT_ISC_INPUT_DISABLE_gc;
 
   AC0.MUXCTRL = AC_MUXPOS_AINP0_gc | AC_MUXNEG_DACREF_gc;
@@ -28,22 +29,22 @@ void AC_init() {
 
 void VREF_init(void) { VREF.ACREF = VREF_REFSEL_1V024_gc; }
 
-void LED_init() { PORTA.DIRSET = PIN2_bm; }
+void LED_init() { PORTA.DIRSET = LED_bm; }
 
 void set_LED_on() {
   // LED is active low. Set pin LOW to turn LED on
-  PORTA.OUTCLR = PIN2_bm;
+  PORTA.OUTCLR = LED_bm;
 }
 
 void set_LED_off() {
   // LED is active low. Set pin HIGH to turn LED off
-  PORTA.OUTSET = PIN2_bm;
+  PORTA.OUTSET = LED_bm;
 }
 
 // Function to initialize sleep mode
-// void sleep_init(void) {
-//   set_sleep_mode(SLEEP_MODE_STANDBY); // Standby sleep mode
-// }
+void sleep_init(void) {
+  set_sleep_mode(SLEEP_MODE_STANDBY); // Standby sleep mode
+}
 
 void TCA0_init() {
   // Set the period of the timer. PER = period[s] * F_CPU / Prescaler = 0.01s *
@@ -76,11 +77,13 @@ ISR(TCA0_OVF_vect) {
 int main() {
   AC_init();
   VREF_init();
+    LED_init();
   // Initialize timer
   TCA0_init();
   // Enable interrupts
   sei();
-    set_sleep_mode(SLEEP_MODE_STANDBY);
+
+    sleep_init();
 
   while (1) {
     sleep_mode();
